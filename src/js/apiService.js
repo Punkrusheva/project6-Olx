0c4c83d6b7b0a0dfb31f282bac4a9607101bee87
kasiian irina <karamio09@gmail.com>
2020-12-09 23:03:50 +0200

    Create apiService.js
     создала файл apiService

---
+++ src/js/apiService.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://callboard-backend.herokuapp.com';
+
+//бяка какя-то
+// export const registerUser = userData => {
+//     const {email, password } = userData;
+  
+//     return fetch(`${BASE_URL}/auth/register`, { name, email, password }).then(res => res.json());
+//   }
