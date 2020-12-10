69773760fd74e53e18f84e8db51d0629032ed708
Kateryna Liubchyk <shevtsova.kateryna@gmail.com>
2020-12-11 00:48:53 +0200

    часть Js

--- src/js/create-ad.js
+++ src/js/create-ad.js
@@ -1,11 +1,36 @@
 import createAdMarkupTpl from '../templates/modal-create-ad.hbs'
-const createAdBtn = document.querySelector(".create-ad-js");
+const openModalCreateAdBtn = document.querySelector('[data-create-ad-modal-open]');
+const closeModalCreateADBtn = document.querySelector('[data-create-ad-modal-close]');
+const createAdModalRef = document.querySelector('[data-create-ad-modal]');
+console.log(createAdModalRef)
 
-console.log(bodyForModal);
 
-createAdBtn.addEventListener('click', onModalCreateAdOpen);
 
-function onModalCreateAdOpen() {
+openModalCreateAdBtn.addEventListener('click', openModalCreateAd);
+closeModalCreateADBtn.addEventListener('click', closeModalCreateAd)
+
+function openModalCreateAd() {
     
-
+    window.addEventListener("keydown", onKeyDown);
+    createAdModalRef.addEventListener("click", onOverlayClick);
+    createAdModalRef.classList.remove("is-hidden");
 }
+
+function closeModalCreateAd() {
+    window.removeEventListener("keydown", onKeyDown);
+    refs.modal.removeEventListener("click", onOverlayClick);
+    refs.modal.classList.add("is-hidden");
+}
+
+function onOverlayClick(event) {
+    if (event.currentTarget === event.target) {
+      onClickCloseModal()
+    }
+}
+  
+function onKeyDown(event) {
+    if (event.code === "Escape") {
+      onClickCloseModal()
+    }
+}
+
