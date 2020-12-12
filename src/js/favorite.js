f5bd622b4fbeda307661c0811a27030ed5d11638
AnastaciaZ <zaikina.anastacia@gmail.com>
2020-12-12 20:14:10 +0200

    full-cren

---
+++ src/js/favorite.js
@@ -0,0 +1,16 @@
+const refs = {
+    addFavoriteBtn: document.querySelector('.product-card-icon-favorite'),
+    removeFavoriteBtn: document.querySelector('.product-card-icon-favorite-user')
+   
+}
+
+console.log(addFavoriteBtn);
+refs.addFavoriteBtn.addEventListener('click', addFavoriteCard);
+refs.removeFavoriteBtn.addEventListener('click', removeFavoriteCard);
+
+function addFavoriteCard() { 
+
+}
+function removeFavoriteCard() { 
+
+}
