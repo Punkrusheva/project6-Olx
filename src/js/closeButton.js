7bf427ffc878041b3b0450a8978a2f126c972fc8
Pankrusheva Anastasiia <50904268+Punkrusheva@users.noreply.github.com>
2020-12-13 12:01:52 +0200

    Revert "Do something"

--- src/js/closeButton.js
+++ src/js/closeButton.js
@@ -1,23 +1,22 @@
- export function onClickOpenModal() {
-    window.addEventListener("keydown", onKeyDown);
-    refs.modal.addEventListener("click", onOverlayClick);
-    refs.modal.classList.remove("is-hidden");
-  };
 
-  export function onClickCloseModal() {
-    window.removeEventListener("keydown", onKeyDown);
-    refs.modal.removeEventListener("click", onOverlayClick);
-    refs.modal.classList.add("is-hidden");
-  };
+     //function onCloseModal() {
+    //     window.removeEventListener('keydown', onEscCloseModal);
+    //     modal.classList.remove('is-open');
+    //   }
+    
+// Закрытие модалки клавишей Escape
 
-  export function onOverlayClick(event) {
-    if (event.currentTarget === event.target) {
-      onClickCloseModal()
-    }
-  };
-  export function onKeyDown(event) {
-    if (event.code === "Escape") {
-      onClickCloseModal()
-    }
-};
-export{onKeyDown, onOverlayClick,onClickCloseModal,onClickOpenModal};
+    // function onEscCloseModal(e) {
+    //     if (e.code === 'Escape') {
+    //       onCloseModal();
+    //     }
+    //   }
+    // Закрытие модалки кликом на оверлей
+    // modal.addEventListener('click', onOverlayAndBtnClick);
+
+    // function onOverlayAndBtnClick(e) {
+    //     if (e.target === overlayEl || e.target === closeBtn) {
+    //       onCloseModal();
+    //     }
+    //   }
+    
