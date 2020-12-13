3ad6d12ab4de24ad7198fa64628060c4f8858a7d
kasiian irina <karamio09@gmail.com>
2020-12-13 21:41:27 +0200

    Update apiService.js

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -6,36 +6,5 @@ export const registerUser = userData => {
   const {email, password } = userData;
 
   return axios.post(`${BASE_URL}auth/register`, { email, password});
-}
+};
 
-//регистрация
-
-// export default async function addregisterUser() {
-//     const options = {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(user),
-//     };
-    
-//     const response = await fetch(`${BASE_URL}auth/register`,options);
-//   const responseJson = await response.json();
-//   return responseJson
-//   }
- 
-//   //авторизация
-//   export  async function fetchAuthorisLogin(BASE_URL, dataRegistry) {
-//     const options = {
-//   method: 'POST',
-//   body: JSON.stringify(dataRegistry),
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'accept': 'application/json',
-//   },
-// }
-//     const response = await fetch(`${BASE_URL}auth/login`,options);
-//     const responseJson = await response.json();
-//     return  localStorage.setItem('key', `${responseJson.accessToken}`);
-
-// }; 
