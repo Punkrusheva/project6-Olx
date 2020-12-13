96638c56c646f3ce6d688161c3fc7e59ae49aaee
kasiian irina <karamio09@gmail.com>
2020-12-13 21:33:00 +0200

    Update apiService.js

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,39 +1,41 @@
+import axios from 'axios';
 const BASE_URL = 'https://callboard-backend.herokuapp.com/';
 
-const user = {
-  email: '{}',
-  password: '{}', 
-};
-console.log(user);
+
+export const registerUser = userData => {
+  const {email, password } = userData;
+
+  return axios.post(`${BASE_URL}auth/register`, { email, password});
+}
 
 //регистрация
 
-export default async function addregisterUser() {
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(user),
-    };
+// export default async function addregisterUser() {
+//     const options = {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(user),
+//     };
     
-    const response = await fetch(`${BASE_URL}auth/register`,options);
-  const responseJson = await response.json();
-  return responseJson
-  }
+//     const response = await fetch(`${BASE_URL}auth/register`,options);
+//   const responseJson = await response.json();
+//   return responseJson
+//   }
  
-  //авторизация
-  export  async function fetchAuthorisLogin(BASE_URL, dataRegistry) {
-    const options = {
-  method: 'POST',
-  body: JSON.stringify(dataRegistry),
-  headers: {
-    'Content-Type': 'application/json',
-    'accept': 'application/json',
-  },
-}
-    const response = await fetch(`${BASE_URL}auth/login`,options);
-    const responseJson = await response.json();
-    return  localStorage.setItem('key', `${responseJson.accessToken}`);
+//   //авторизация
+//   export  async function fetchAuthorisLogin(BASE_URL, dataRegistry) {
+//     const options = {
+//   method: 'POST',
+//   body: JSON.stringify(dataRegistry),
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'accept': 'application/json',
+//   },
+// }
+//     const response = await fetch(`${BASE_URL}auth/login`,options);
+//     const responseJson = await response.json();
+//     return  localStorage.setItem('key', `${responseJson.accessToken}`);
 
-}; 
+// }; 
