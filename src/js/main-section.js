bdf66801e914460530f41c1e9ffad544da71a5b5
Vitalii Speka <spekava@gmail.com>
2020-12-13 15:05:30 +0200

    +

--- src/js/main-section.js
+++ src/js/main-section.js
@@ -13,7 +13,7 @@ const mainContainerRef = document.querySelector('.main-container');
 // mainContainerRef.innerHTML = oneCard(fetchAllCards);
 // mainContainerRef.innerHTML = fetchAllCards;
 /* все объявления одной категории  */ 
-mainContainerRef.innerHTML = oneCategory(fetchAllCards);
+// mainContainerRef.innerHTML = oneCategory(fetchAllCards);
 
 
 /* API test  */
@@ -27,20 +27,42 @@ const requestOptions = {
 
 const fetchAllCards = fetch(`${BASE_URL}call?page=${currentPage}`, requestOptions)
     .then(response => response.json())
-    .then(result => {
-        // console.dir(result.free);
-        console.log(result);
-        // const names = result.map(response => console.log(response.free));
-        // console.log(names);
-        const markup = allCategory(result);
-        // console.log(markup);
-        // mainContainerRef.insertAdjacentHTML('afterbegin', markup);
-        mainContainerRef.innerHTML = markup;
+    // .then(result => {
+    //     // console.dir(result.free);
+    //     console.log(result);
+    //     // const names = result.map(response => console.log(response.free));
+    //     // console.log(names);
+    //     const markup = allCategory(result);
+    //     // console.log(markup);
+    //     // mainContainerRef.insertAdjacentHTML('afterbegin', markup);
+    //     mainContainerRef.innerHTML = markup;
 
-    })
+    // })
     .catch(error => console.log('error', error));
                 
 console.log(fetchAllCards);
+
+function renderAllCardAndAllCategory() {
+    fetchAllCards.then(result => {
+        console.log(result); 
+        // console.dir(result.free);
+        
+    });
+    //     .then(result => {
+    //     result.map(res => { console.log(res); }
+        
+    //     // const answer = )
+    //     // console.dir(answer);
+    //     // // const names = result.map(response => console.log(response.free));
+    //     // // console.log(names);
+    //     // const markup = oneCategory(result);
+    //     // // console.log(markup);
+    //     // // mainContainerRef.insertAdjacentHTML('afterbegin', markup);
+    //     // mainContainerRef.innerHTML = markup[6];
+    // );
+}
+
+renderAllCardAndAllCategory();
 
 // const users = [
 //   { name: 'Mango', isActive: true },
