385b7cf38d2ace0c10daf93393fc5ad3848e5a0d
Maria <mashero4ka@gmail.com>
2020-12-14 16:09:07 +0200

    changed modal-name

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import mainSection from './js/main-section';
 import './js/main-section';
 
 import './js/modal';
-// console.log(mainSection);
+console.log(mainSection);
 
-// import './js/create-ad-open-close';
-// import './js/create-ad';
+import './js/create-ad-open-close';
+import './js/create-ad';
 
-// import './js/apiService';
+import './js/apiService';
 
 import './js/registration';
 import './js/auth-check';
-// import './js/test';
+import './js/test';
 
 import './js/favorite';
 import './js/search';
