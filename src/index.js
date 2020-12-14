4f5b63ce4802da469a2bbd9346ed20e3fb783deb
Maria <mashero4ka@gmail.com>
2020-12-14 15:34:28 +0200

    trying opening modal

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import './js/main-section';
 import './js/modal';
 // console.log(mainSection);
 
-import './js/create-ad-open-close';
-import './js/create-ad';
+// import './js/create-ad-open-close';
+// import './js/create-ad';
 
-import './js/apiService';
+// import './js/apiService';
 
 import './js/registration';
 import './js/auth-check';
-import './js/test';
+// import './js/test';
 
 import './js/favorite';
 import './js/search';
--- src/js/modal-one-card.js
+++ src/js/modal-one-card.js
@@ -1,22 +1,46 @@
+import openCard from '../templates/modal-one-card.hbs';
+
+
 const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn: document.querySelectorAll('.product-card-icon-fullscreen'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('.product-card-icon-fullscreen'),
+    modal: document.querySelector('[data-modal]'),
+
   };
 
-  refs.openModalBtn.addEventListener('click', onClickOpenModal);
+  refs.openModalBtn.forEach(icon =>
+    icon.addEventListener("click", onClickOpenModal)
+  );
+
   refs.closeModalBtn.addEventListener('click', onClickCloseModal);
 
 
-  function onClickOpenModal() {
+function onClickOpenModal() {
+    console.log('panda');
+    try {
+
+        document.body.insertAdjacentHTML("afterbegin", openCard());
     window.addEventListener("keydown", onKeyDown);
     refs.modal.addEventListener("click", onOverlayClick);
     refs.modal.classList.remove("is-hidden");
+    }
+
+    catch (error) {
+        console.log(error);
+    }
+
   }
 
-  function onClickCloseModal() {
+function onClickCloseModal() {
+    console.log('todo1');
+    refs.openModalBtn.forEach(icon =>
+        icon.removeEventListener("click", onClickOpenModal)
+    );
+    console.log('todo1');
     window.removeEventListener("keydown", onKeyDown);
-    refs.modal.removeEventListener("click", onOverlayClick);
+      refs.modal.removeEventListener("click", onOverlayClick);
+      modal.remove();
+
     refs.modal.classList.add("is-hidden");
   }
 
