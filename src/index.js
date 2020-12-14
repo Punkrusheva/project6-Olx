5a5a3d4523da364ff7f30952f00eb7482ea73952
Pankrusheva Anastasiia <a.pankrusheva@gmail.com>
2020-12-14 10:07:38 +0200

    fixes 6

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './sass/main.scss';
 import './js/menu'
-import './js/search_modal'
+import './js/search-modal'
 import mainSection from './js/main-section';
 import './js/main-section';
 
@@ -10,9 +10,6 @@ import './js/modal';
 import './js/create-ad-open-close';
 import './js/create-ad';
 
-
-
-
 import './js/apiService';
 
 import './js/registration'; 
@@ -20,12 +17,4 @@ import './js/auth-check';
 import './js/test'; 
 
 import './js/favorite';
-import './js/serch'
-
-
-
-
-
-
-
-
+import './js/search';
