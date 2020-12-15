5d5d143bac91e0b074fa780b90ab958de4bc6976
Nata Pryhunova <prygunova@gmail.com>
2020-12-15 07:06:25 +0200

    Добавила api для регистрации и логина, саму регистрацию, неполный логин

--- src/index.js
+++ src/index.js
@@ -23,3 +23,8 @@ import './js/search';
 import './js/test'; 
 import './js/bad-url'; 
 import './js/modal-one-card';
+
+import './js/auth-modal/authApi';
+// import './js/auth-modal/authLogin';
+import './js/auth-modal/authRegistr';
+import './js/auth-modal/pnotify';
