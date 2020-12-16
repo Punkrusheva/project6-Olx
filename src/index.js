68b25de6b23b7b5f6a8222ce641de6089a107402
Pankrusheva Anastasiia <a.pankrusheva@gmail.com>
2020-12-16 23:31:27 +0200

    Revert "Merge branch 'maria' into Fixes"
    This reverts commit f4d39f0ef0ae923d3ec809debc3c11cad225a070, reversing
    changes made to 222e250c414d44dcc2379ef73409334b8b4c912c.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import 'material-design-icons/iconfont/material-icons.css';
 import './sass/main.scss';
 
 import './js/search-modal'
@@ -12,21 +11,19 @@ import './js/main-section';
 import './js/create-ad-open-close';
 import './js/create-ad';
 
-import './js/modal-one-card';
-
 // import './js/apiService';
 
 //import './js/registration';
 //import './js/auth-check';
 
 import './js/router';
-import './js/bad-url';
+import './js/bad-url'; 
 
 import './js/favorites/favorite';
 import './js/search';
 
-import './js/test';
-
+import './js/test'; 
+import './js/modal-one-card';
 
 import './js/auth-modal/authApi';
 // import './js/auth-modal/authLogin';
--- src/js/modal-one-card.js
+++ src/js/modal-one-card.js
@@ -1,33 +1,11 @@
 import openCard from '../templates/modal-one-card.hbs';
 console.log(openCard);
 
-document.body.addEventListener('click', openInfoAboutSeller);
-
-const aboutSellerContOpened = document.querySelector(
-    '.modal-button-box-info');
-
-const aboutSellerContClosed = document.querySelector('.modal-button-box');
-
-function openInfoAboutSeller(evt) {
-    if (evt.target.classList.contains('modal-button-box')) {
-        {
-            aboutSellerContClosed.style.opacity = '0';
-            aboutSellerContOpened.style.opacity = '1';
-
-            setTimeout(() => {
-                console.log(aboutSellerContClosed);
-                aboutSellerContClosed.style.opacity = '1';
-                aboutSellerContOpened.style.opacity = '0';
-            }, 2500);
-          }
-    };
-}
-
 const refs = {
     openModalBtn: document.querySelectorAll('.product-card-icon-fullscreen'),
     // parentCloseModalBtn: document.querySelector('.open-card'),
-    сloseModalBtn: document.querySelector('[data-close]'),
-    modal: document.querySelector('[data-modal-one]'),
+    сloseModalBtn: document.querySelector('.close-form'),
+    modal: document.querySelector('[data-modal]'),
     cont: document.getElementsByTagName('.open-card'),
 
   };
@@ -38,7 +16,7 @@ async function startListening() {
   );
 }
 
-async function onClickOpenModal(ev) {
+function onClickOpenModal(ev) {
 
   ev.preventDefault();
 
