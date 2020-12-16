b87af2a069c9fa428c50e87c89865f6820377ddd
Maria <mashero4ka@gmail.com>
2020-12-16 18:18:07 +0200

    fixed some mismatches

--- src/index.js
+++ src/index.js
@@ -10,19 +10,21 @@ import './js/main-section';
 import './js/create-ad-open-close';
 import './js/create-ad';
 
+import './js/modal-one-card';
+
 // import './js/apiService';
 
 //import './js/registration';
 //import './js/auth-check';
 
 import './js/router';
-import './js/bad-url'; 
+import './js/bad-url';
 
 import './js/favorites/favorite';
 import './js/search';
 
-import './js/test'; 
-import './js/modal-one-card';
+import './js/test';
+
 
 import './js/auth-modal/authApi';
 // import './js/auth-modal/authLogin';
