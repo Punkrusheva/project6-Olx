602f2d0d7c385d65dae212cc0335626796ab88f6
Maria <mashero4ka@gmail.com>
2020-12-16 20:00:56 +0200

    added seller info

--- src/js/modal-one-card.js
+++ src/js/modal-one-card.js
@@ -1,11 +1,33 @@
 import openCard from '../templates/modal-one-card.hbs';
 console.log(openCard);
 
+document.body.addEventListener('click', openInfoAboutSeller, )
+
+const aboutSellerContOpened = document.querySelector(
+    '.modal-button-box-info');
+
+const aboutSellerContClosed = document.querySelector('.modal-button-box');
+
+function openInfoAboutSeller(evt) {
+    if (evt.target.classList.contains('modal-button-box')) {
+        {
+            aboutSellerContClosed.style.opacity = '0';
+            aboutSellerContOpened.style.opacity = '1';
+
+            setTimeout(() => {
+                console.log(aboutSellerContClosed);
+                aboutSellerContClosed.style.opacity = '1';
+                aboutSellerContOpened.style.opacity = '0';
+            }, 2500);
+          }
+    };
+}
+
 const refs = {
     openModalBtn: document.querySelectorAll('.product-card-icon-fullscreen'),
     // parentCloseModalBtn: document.querySelector('.open-card'),
-    сloseModalBtn: document.querySelector('.close-form'),
-    modal: document.querySelector('[data-modal]'),
+    сloseModalBtn: document.querySelector('[data-close]'),
+    modal: document.querySelector('[data-modal-one]'),
     cont: document.getElementsByTagName('.open-card'),
 
   };
@@ -16,7 +38,7 @@ async function startListening() {
   );
 }
 
-function onClickOpenModal(ev) {
+async function onClickOpenModal(ev) {
 
   ev.preventDefault();
 
