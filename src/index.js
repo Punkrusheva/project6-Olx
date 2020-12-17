c36c99f2250ce0432a05858b73623c1e64b3bb2e
Pankrusheva Anastasiia <a.pankrusheva@gmail.com>
2020-12-17 14:30:46 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,26 +5,26 @@ import './js/menu'
 import './js/user-button'
 
 import './js/main-section';
-// // import './js/carousel';
+//import './js/carousel';
 
-// //import './js/modal';
+import './js/modal';
 
-// import './js/create-ad-open-close';
-// import './js/create-ad';
+import './js/create-ad-open-close';
+import './js/create-ad';
 
-// // import './js/apiService';
+import './js/apiService';
 
-// //import './js/registration';
-// //import './js/auth-check';
+import './js/registration';
+import './js/auth-check';
 
-// import './js/router';
-// import './js/bad-url'; 
+import './js/router';
+import './js/bad-url'; 
 
-// import './js/favorites/favorite';
+import './js/favorites/favorite';
 import './js/search';
 
 // import './js/test'; 
-// import './js/modal-one-card';
+import './js/modal-one-card';
 
 import './js/auth-modal/authApi';
 import './js/auth-modal/authLogin';
