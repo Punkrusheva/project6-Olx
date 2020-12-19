b124f7c184f3f1aba9be7af1086d77a8fcdbc52f
kasiian irina <karamio09@gmail.com>
2020-12-19 15:37:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './js/menu';
 import './js/user-button';
 
 import './js/main-section';
-import './js/createAd/auth-check'
+
 
 //import './js/carousel';
 
@@ -29,12 +29,12 @@ import './js/bad-url';
 
 // import './js/modal-one-card';
 
-// import './js/auth-modal/authApi';
-// import './js/auth-modal/authLogin';
-// import './js/auth-modal/authRegistr';
-// import './js/auth-modal/pnotify';
-// import './js/auth-modal/auth';
-// import './js/test'; 
+import './js/auth-modal/authApi';
+import './js/auth-modal/authLogin';
+import './js/auth-modal/authRegistr';
+import './js/auth-modal/pnotify';
+import './js/auth-modal/auth';
+import './js/test'; 
 import './js/my-favoite';
 
 
