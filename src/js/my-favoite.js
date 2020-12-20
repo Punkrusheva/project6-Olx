c7f9a4d2e740267767a8461aae9b4c09e22ac659
kasiian irina <karamio09@gmail.com>
2020-12-20 16:21:03 +0200

    Update my-favoite.js

--- src/js/my-favoite.js
+++ src/js/my-favoite.js
@@ -21,8 +21,16 @@ fetchGetFavorites()
 const showMyFavourites = document.querySelector('.favor');
 showMyFavourites.addEventListener('click', showFavouritesCards);
 
-function showFavouritesCards(){
-    fetchGetFavorites(URL).then(render => document.querySelector('.main-container').innerHTML = myFavoriteTpt( render.favourites))
+// function showFavouritesCards(){
+//     fetchGetFavorites(URL).then(render => document.querySelector('.main-container').innerHTML = myFavoriteTpt( render.favourites))
+// }
+function showFavouritesCards(e) {
+  fetchGetFavorites(URL).then(render => {
+    if (render.favourites.length === 0) {
+      const main = document.querySelector('.main-container')
+     main.innerHTML = '<p>Ничего не найдено</p>'
+    } else {
+      document.querySelector('.container').innerHTML = myFavoriteTpt(render.favourites)
+    }
+  })
 }
-
-
