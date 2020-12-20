7d09f977e626b590322170c3d09612c2c283ca8b
Dmytro Vasylenko <tredstoun651@uk.net>
2020-12-20 14:56:11 +0200

    исправил ошибки поиска и позицию кнопки добавить

--- src/js/search.js
+++ src/js/search.js
@@ -4,38 +4,39 @@ import productCardTpl from '../templates/product-cards.hbs'
 export default SearchProducts()
 
 function SearchProducts() {
-const BASE_URL = 'https://callboard-backend.herokuapp.com'
+    const BASE_URL = 'https://callboard-backend.herokuapp.com'
 
- class FindProduct {
-    constructor() {
-        this.searchQuery = '';
+    class FindProduct {
+        constructor() {
+            this.searchQuery = '';
+        }
+
+        async fetchProducts() {
+            const url = `${BASE_URL}/call/find?&search=${this.searchQuery}`
+            const fetches = await fetch(url)
+            const json = await fetches.json()
+
+            if (json.length === 0) {
+                throw 'Ничего не найдено'
+            }
+            return json;
+        }
+
+        get query() {
+            return this.searchQuery;
+        }
+        set query(newSearchQuery) {
+            this.searchQuery = newSearchQuery
+        }
     }
 
-   async fetchProducts() {
-       const url = `${BASE_URL}/call/find?&search=${this.searchQuery}`
-       const fetches = await fetch(url)
-       const json = await fetches.json()  
-       if (json.length === 0) {
-         throw 'Ничего не найдено'
-       }
-       return json;
-    }
-
-    get query() {
-        return this.searchQuery;
-    }
-    set query(newSearchQuery) {
-        this.searchQuery = newSearchQuery
-    }
-}
-
-const searchForm = document.querySelector('[data-search-form]')
-const searchContainer = document.querySelector('.main-container')
+    const searchForm = document.querySelector('[data-search-form]')
+    const searchContainer = document.querySelector('.main-container')
     const modalSearch = document.querySelector('[data-search-modal]')
     const input = document.querySelector('.search__input')
     const error1 = document.querySelector('.error-text')
     
-const products = new FindProduct()
+    const products = new FindProduct()
 
     searchForm.addEventListener('submit', onSearch);
     input.addEventListener('focus', onFocusInput);
@@ -46,20 +47,21 @@ const products = new FindProduct()
     }
     
 
-function onSearch(e) {
-    e.preventDefault();
-    products.query = e.currentTarget.elements.query.value.trim()
+    function onSearch(e) {
+        e.preventDefault();
+        products.query = e.currentTarget.elements.query.value.trim()
 
-    if (products.query === '') {
-        error1.textContent = 'Введите что-нибуть!'
-        error1.classList.remove('is-hidden')
+        if (products.query === '') {
+            error1.textContent = 'Введите что-нибуть!'
+            error1.classList.remove('is-hidden')
+            return
+        }
+        fetchProductCard()
     }
-    fetchProductCard()
-}
 
-function appendCardMarkup(card) {
-    searchContainer.innerHTML = `${productCardTpl(card)}`
-}
+    function appendCardMarkup(card) {
+        searchContainer.innerHTML = `${productCardTpl(card)}`
+    }
 
     async function fetchProductCard() {
         try {
@@ -67,19 +69,20 @@ function appendCardMarkup(card) {
             console.log(product)
             appendCardMarkup(product)
             modalSearch.classList.add("is-hidden")
-        } catch {
-         errors(error)
+        } catch (error) {
+            errors(error)
         }
     }
     
     function errors(error) {
         if (error === 'Ничего не найдено') {
             error1.textContent = 'По этому запросу ничего не найдено.'
-             error1.classList.remove('is-hidden')
-        } else
-            error1.textContent = 'По этому запросу ничего не найдено.'
-         error1.classList.remove('is-hidden')
-}
+            error1.classList.remove('is-hidden')
+        } else {
+            error1.textContent = 'Не удалось загрузить товар'
+            error1.classList.remove('is-hidden')
+        }
+    }
 }
 
 
