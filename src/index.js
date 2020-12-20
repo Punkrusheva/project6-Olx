1d0ac17842c3d85e5e1a5ee7531883b84f2418b0
Nata Pryhunova <prygunova@gmail.com>
2020-12-20 15:16:39 +0200

    удалила лишние файлы и переместила в index.js файлы авторизации

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ import './js/search';
 import './js/menu'
 import './js/user-button'
 
+import './js/auth-modal/authApi';
+import './js/auth-modal/authLogin';
+import './js/auth-modal/authRegistr';
+import './js/auth-modal/pnotify';
+import './js/auth-modal/auth';
+
+
 import './js/main-section';
 //import './js/carousel';
 
@@ -33,11 +40,4 @@ import './js/favorites/favorite';
 
 // import './js/test'; 
 import './js/modal-one-card';
-
-// import './js/auth-modal/authApi';
-// import './js/auth-modal/authLogin';
-// import './js/auth-modal/authRegistr';
-// import './js/auth-modal/pnotify';
-// import './js/auth-modal/auth';
-
 
